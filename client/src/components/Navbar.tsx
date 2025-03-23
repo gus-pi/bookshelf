7038f678c09caeb2be2d77c19b1b997bf535bfb8
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-23 11:18:43 -0400

    add navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,36 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar bg-amber-900 text-slate-100">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-3xl ">
+          Bookshelf
+          <span className="text-amber-950 font-bold -ml-1">.</span>
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/shelves">Shelves</Link>
+          </li>
+          <li>
+            <details>
+              <summary>Account</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li>
+                  <Link to="/account">Edit account</Link>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

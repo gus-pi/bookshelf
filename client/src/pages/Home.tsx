13ae3ed829798d7172b65bceb0612ed62928fa10
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-24 07:49:16 -0400

    start home psge

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col justify-center items-center mt-5">
+      <div className="flex flex-col ">
+        <h1 className="text-5xl">Bookshelf</h1>
+        <p className="text-xs ml-auto">by Gustavo Pinedo</p>
+      </div>
+      <p className="mt-5">
+        Organize your own personal bookshelf and take a peak at what others are
+        reading as well.
+      </p>
+    </div>
+  );
 };
 
 export default Home;

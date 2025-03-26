5209626644e1e6c484557eb27247a5a4c2f5e1fb
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-26 10:09:25 -0400

    fix login component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,11 +1,11 @@
 import { AuthContext } from '@/context/AuthContext';
-import axios from 'axios';
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(''); // Add error state
   const navigate = useNavigate();
 
   const authContext = useContext(AuthContext);
@@ -14,10 +14,11 @@ const Login = () => {
     throw new Error('AuthContext.Provider is missing!');
   }
 
-  const { userCredentials, setUserCredentials } = authContext;
+  const { setUserCredentials } = authContext;
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setError(''); // Clear previous errors
     try {
       const response = await fetch(`http://localhost:5000/api/users/login`, {
         method: 'POST',
@@ -25,27 +26,40 @@ const Login = () => {
         body: JSON.stringify({ email, password }),
       });
       const data = await response.json();
-      setUserCredentials(data.user);
-      console.log(userCredentials);
-      navigate('/'); // Redirect to home
+      if (response.ok) {
+        setUserCredentials(data.user);
+        navigate('/'); // Redirect to home
+      } else {
+        setError(data.message || 'Login failed');
+      }
     } catch (error: any) {
-      console.error(
-        'Login failed:',
-        error.response?.data?.message || error.message
+      setError(
+        error.response?.data?.message || error.message || 'Login failed'
       );
     }
   };
+
   return (
     <div className="bg-amber-900 text-slate-100">
       <h1>Login</h1>
+      {error && <p className="text-red-500">{error}</p>}{' '}
+      {/* Display error message */}
       <form onSubmit={handleSubmit}>
         <label>email</label>
-        <input type="text" className="bg-white text-slate-900" name="email" />
+        <input
+          type="text"
+          className="bg-white text-slate-900"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label>password</label>
         <input
           type="password"
           className="bg-white text-slate-900"
           name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button
           type="submit"

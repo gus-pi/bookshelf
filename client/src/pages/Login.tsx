9358523927306703f2b48d9270b29e1649f2b14d
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-28 00:41:08 -0400

    improve login component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -38,31 +38,77 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-amber-900 text-slate-100">
-      <h1>Login</h1>
+    <div className="text-slate-100 flex flex-col items-center bg-black">
+      <h1 className="text-5xl mt-20">Login</h1>
       {error && <p className="text-red-500">{error}</p>}{' '}
-      {/* Display error message */}
-      <form onSubmit={handleSubmit}>
-        <label>email</label>
-        <input
-          type="text"
-          className="bg-white text-slate-900"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>password</label>
-        <input
-          type="password"
-          className="bg-white text-slate-900"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="bg-amber-600 rounded-md px-2 mx-3 hover:cursor-pointer"
-        >
+      <form onSubmit={handleSubmit} className="flex flex-col mt-10">
+        <label className="input validator mb-1">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+            </g>
+          </svg>
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <div className="validator-hint hidden">Enter valid email address</div>
+
+        <label className="input validator">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+              <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+            </g>
+          </svg>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="Password"
+            // minLength={6}
+            // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            // title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+          />
+        </label>
+        <p className="validator-hint hidden">
+          Must be more than 8 characters, including
+          <br />
+          At least one number
+          <br />
+          At least one lowercase letter
+          <br />
+          At least one uppercase letter
+        </p>
+        <button type="submit" className="btn btn-soft btn-secondary my-5">
           submit
         </button>
       </form>

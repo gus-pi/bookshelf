e96512b10a8ca6ba820a55b16c4e0565cc3ffe17
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-04-14 10:25:54 -0400

    fix cors origin typo

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin:
       //'http://localhost:3000'
-      'https://bookshelfclient.vercel.app/',
+      'https://bookshelfclient.vercel.app',
     credentials: true,
   })
 );
